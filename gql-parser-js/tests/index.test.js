6ec619300d4a56499b152f2e5d5c4f5fdb135e95
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2021-11-03 18:36:17 +0100

    jsonify location objects

--- gql-parser-js/tests/index.test.js
+++ gql-parser-js/tests/index.test.js
@@ -3,12 +3,102 @@ const path = require("path");
 const { parse: parse_reference } = require("graphql/language");
 const { parse } = require("..");
 
+function jsonifyLocs(node) {
+  return { ...node, loc: node.loc.toJSON() };
+}
+
+function transformNode(node) {
+  switch (node.kind) {
+    case "Argument":
+      return jsonifyLocs({
+        ...node,
+        name: transformNode(node.name),
+        value: transformNode(node.value),
+      });
+    case "BooleanValue":
+      return jsonifyLocs(node);
+    case "Directive":
+      return jsonifyLocs({
+        ...node,
+        name: transformNode(node.name),
+        arguments: node.arguments.map(transformNode),
+      });
+    case "Document":
+      return jsonifyLocs({
+        ...node,
+        definitions: node.definitions.map(transformNode),
+      });
+    case "EnumValue":
+      return jsonifyLocs(node);
+    case "Field":
+      return jsonifyLocs({
+        ...node,
+        alias: node.alias ? transformNode(node.alias) : node.alias,
+        name: transformNode(node.name),
+        arguments: node.arguments.map(transformNode),
+        directives: node.directives.map(transformNode),
+        selectionSet: node.selectionSet
+          ? transformNode(node.selectionSet)
+          : node.selectionSet,
+      });
+    case "FloatValue":
+      return jsonifyLocs(node);
+    case "IntValue":
+      return jsonifyLocs(node);
+    case "ListValue":
+      return jsonifyLocs({ ...node, values: node.values.map(transformNode) });
+    case "Name":
+      return jsonifyLocs(node);
+    case "NamedType":
+      return jsonifyLocs({ ...node, name: transformNode(node.name) });
+    case "NullValue":
+      return jsonifyLocs(node);
+    case "ObjectField":
+      return jsonifyLocs({
+        ...node,
+        name: transformNode(node.name),
+        value: transformNode(node.value),
+      });
+    case "ObjectValue":
+      return jsonifyLocs({ ...node, fields: node.fields.map(transformNode) });
+    case "OperationDefinition":
+      return jsonifyLocs({
+        ...node,
+        name: node.name ? transformNode(node.name) : node.name,
+        variableDefinitions: node.variableDefinitions.map(transformNode),
+        directives: node.directives.map(transformNode),
+        selectionSet: transformNode(node.selectionSet),
+      });
+    case "SelectionSet":
+      return jsonifyLocs({
+        ...node,
+        selections: node.selections.map(transformNode),
+      });
+    case "StringValue":
+      return jsonifyLocs(node);
+    case "Variable":
+      return jsonifyLocs({ ...node, name: transformNode(node.name) });
+    case "VariableDefinition":
+      return jsonifyLocs({
+        ...node,
+        variable: transformNode(node.variable),
+        type: transformNode(node.type),
+        defaultValue: node.defaultValue
+          ? transformNode(node.defaultValue)
+          : node.defaultValue,
+        directives: node.directives.map(transformNode),
+      });
+    default:
+      throw new Error(`Unknown kind "${node.kind}"`);
+  }
+}
+
 describe("parse", () => {
   it("should work", async () => {
     const query = await fs.promises.readFile(
       path.join(__dirname, "example.gql"),
       "utf8"
     );
-    expect(parse(query)).toEqual(parse_reference(query));
+    expect(parse(query)).toEqual(transformNode(parse_reference(query)));
   });
 });
